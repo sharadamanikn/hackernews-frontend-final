ab9c1e51fe1356a122828b4bd6a35486a74d5c09
Sharadamani K N <sharadamanikn@gmail.com>
2025-05-09 16:25:05 +0530

    feat: update betterAuthClient to use dynamic API base URL

--- app/lib/integrations/better-auth/index.ts
+++ app/lib/integrations/better-auth/index.ts
@@ -2,9 +2,11 @@ import { nextCookies } from "better-auth/next-js";
 import { createAuthClient } from "better-auth/react";
 import { usernameClient } from "better-auth/client/plugins";
 
+const API_BASE = process.env.NEXT_PUBLIC_API_BASE_URL;
 export const betterAuthClient = createAuthClient({
-  baseURL: "http://localhost:3000",
+  baseURL:API_BASE,
   plugins: [nextCookies(), usernameClient()],
 });
+
 
 
